27deec61bfe63c789cf155441fa6366a41ceeb6b
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2022-09-07 14:04:57 -0300

    Initial commit with GetX initial settings.

---
+++ android/app/src/main/kotlin/br/com/devclassricardo/getx_dependencies/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.devclassricardo.getx_dependencies
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'pages/home/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'GetX Dependencies',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/home',
+      getPages: [
+        GetPage(name: '/home', page: HomePage.new),
+      ],
+    );
+  }
+}
